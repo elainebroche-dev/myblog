f2aee34daf6a029d88ad3f3abf365ddbb4116205
elainebroche-dev <elainebroche@gmail.com>
2024-09-10 15:42:14 +0100

    intro to views

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,6 @@ class PostList(generic.ListView):
     #model = Post
 
     # alternative way to code
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    #queryset = Post.objects.filter(author=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1) 
+    #template_name = "post_list.html"
