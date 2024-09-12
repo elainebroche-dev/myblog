eddd644c1a8fed67d4658040b460eaad69f980ad
elainebroche-dev <elainebroche@gmail.com>
2024-09-12 14:26:17 +0100

    enable serving of static files

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    #model = Post
-
-    # alternative way to code
-    #queryset = Post.objects.filter(author=1).order_by("-created_on")
     queryset = Post.objects.filter(status=1) 
-    #template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
