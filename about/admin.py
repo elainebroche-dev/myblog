e6e0f86dc46db4244e93b14781b621771ebf0283
elainebroche-dev <elainebroche@gmail.com>
2024-09-13 12:16:55 +0100

    add about

--- about/admin.py
+++ about/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import About
+#from .models import About
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(About)
-class AboutAdmin(SummernoteModelAdmin):
+#@admin.register(About)
+#class AboutAdmin(SummernoteModelAdmin):
 
-    summernote_fields = ('content',)
+   # summernote_fields = ('content',)
--- about/views.py
+++ about/views.py
@@ -4,13 +4,13 @@ from .models import About
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
+#   """
+#    Renders the About page
+#    """
     about = About.objects.all().order_by('-updated_on').first()
 
     return render(
         request,
         "about/about.html",
         {"about": about},
-    )
+   )
